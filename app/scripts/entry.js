9080fb769cdfaf86633d6c8274331bdac7f29d39
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-07-18 16:42:19 -0500

    things basically work but are ugly in all senses of the word

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import Bb from 'backbone';
+import _ from 'underscore';
 
 import settings from './settings';
 
@@ -14,6 +15,19 @@ $('main').append(`
   <input type="password" id="password" value="" placeholder="PASSWORD">
   <input type="button" id="submitlogin" data-login="/login" value="SUBMIT">
   `);
+
+let authToken = '';
+
+const Person = Bb.Model.extend({
+  urlRoot: `https://baas.kinvey.com/appdata/${settings.appId}/people`,
+});
+
+const personList = Bb.Collection.extend({
+  url: `https://baas.kinvey.com/appdata/${settings.appId}/people`,
+  model: Person
+});
+
+let rolodex = new personList ();
 
 function firstPageButton() {
   let login = '';
@@ -33,11 +47,87 @@ function firstPageButton() {
     },
     success: function (resp) {
       console.log('it sent, babyyy');
-      // renderAddressPage();
+      authToken = resp._kmd.authtoken;
+      renderAddressPage();
     },
     error: function () {
       console.log('something is mad');
     }
+  });
+}
+
+function renderAddressPage () {
+
+  $('main').empty().append(`
+    <div class="newperson">
+      <input type="text" id="newname" value="" placeholder="Full Name">
+      <input type="text" id="newnick" value="" placeholder="Nickname">
+      <input type="text" id="newemail" value="" placeholder="Email Address">
+      <input type="text" id="newphone" value="" placeholder="Phone Number">
+      <input type="button" id="submitnewperson" value="SUBMIT">
+    </div>
+    <section class="sidebar">
+    </section>
+    <section class="personwindow">
+    </section>
+    `);
+
+  rolodex.fetch({
+    contentType: 'application/json',
+    headers: {
+      Authorization: 'Kinvey ' + authToken
+    },
+    success: function () {
+      console.log('I Live');
+    },
+    error: function () {
+      console.log('I Die');
+    }
+  });
+
+  rolodex.on('add', function(){
+    $('.sidebar').empty();
+    rolodex.models.forEach(function(person, i){
+      $('.sidebar').append(`<a class="displayperson" data-index="${i}">${person.get('nick')}</a>`);
+    });
+    $('.displayperson').click(function(){
+      let currentIndex = $(this).data().index;
+      // console.log($(this).data().index);
+      $('.personwindow').empty().append(`
+        <div class="personholder" data-index="${currentIndex}">
+          <h3>${rolodex.models[currentIndex].get('name')}</h3>
+          <p>Nickname: ${rolodex.models[currentIndex].get('nick')}</p>
+          <p>Email: ${rolodex.models[currentIndex].get('email')}</p>
+          <p>Phone: ${rolodex.models[currentIndex].get('phone')}</p>
+        </div>
+        `);
+    });
+  });
+
+
+  $('#submitnewperson').click(function(){
+    console.log('wow wheeeeee');
+    let currentPerson = new Person({
+      name: $('#newname').val(),
+      nick: $('#newnick').val(),
+      email: $('#newemail').val(),
+      phone: $('#newphone').val(),
+    });
+    console.log(currentPerson);
+    console.log(authToken);
+    currentPerson.save(null, {
+      contentType: 'application/json',
+      headers: {
+        Authorization: 'Kinvey ' + authToken
+      },
+      success: function () {
+        console.log('I Live');
+      },
+      error: function () {
+        console.log('I Die');
+      }
+    });
+    rolodex.add(currentPerson);
   });
 }
 
